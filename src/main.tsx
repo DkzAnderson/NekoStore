043d37e25c65493b6811a28fd1be1c7b8811e2f8
DkzAnderson <syndrax20@gmail.com>
2025-01-10 21:10:33 -0500

    cambio de 'BrowserRouter' por 'HashRouter'

--- src/main.tsx
+++ src/main.tsx
@@ -1,34 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
+import './index.css';
+import './App.css';
+import App from './App.tsx';
+import { RouterProvider, createHashRouter } from 'react-router-dom'; // Cambiado a createHashRouter
+import { LoginMain } from './Components/Login/LoginMain.tsx';
+import { RegisterMain } from './Components/Register/RegisterMain.tsx';
+import { Start } from './Components/StartApp/Start.tsx';
+import { Profile } from './Components/User/Profile.tsx';
 
-import './index.css'
-import './App.css'
-import App from './App.tsx'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import { LoginMain } from './Components/Login/LoginMain.tsx'
-import { RegisterMain } from './Components/Register/RegisterMain.tsx'
-import { Start } from './Components/StartApp/Start.tsx'
-import { Profile } from './Components/User/Profile.tsx'
-
-const router = createBrowserRouter(
+const router = createHashRouter(
   [
     {
       path: '/',
-      element: <App/>,
+      element: <App />,
       children: [
-        { index: true, element: <Start/> },
-        { path: '/login', element: <LoginMain/> },
-        { path: '/register', element: <RegisterMain/> },
-        { path: '/edit-profile', element: <Profile/> }  
-      ]
+        { index: true, element: <Start /> },
+        { path: '/login', element: <LoginMain /> },
+        { path: '/register', element: <RegisterMain /> },
+        { path: '/edit-profile', element: <Profile /> },
+      ],
     },
   ],
-)
-
+);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
